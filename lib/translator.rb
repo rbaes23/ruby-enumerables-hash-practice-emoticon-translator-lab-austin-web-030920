7ce169e973f7e6fffd348b9fbba1508c75efff6d
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-02 18:53:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   result = load_library(file_path)[:get_emoticon][emoticon]
-  result ? result: "Sorry, that emoticon was not found"
+  if emoticon != result
+    puts "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
