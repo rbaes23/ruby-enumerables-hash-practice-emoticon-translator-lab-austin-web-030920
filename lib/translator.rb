5482558b51cd90ea56cc060495a3bb3846994ae0
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-02 18:57:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,5 +23,5 @@ end
 
 def get_english_meaning(file_path, emoticon)
   result = load_library(file_path)[:get_meaning][emoticon]
-  result
+  result ? result: "Sorry, that emoticon was not found"
 end
